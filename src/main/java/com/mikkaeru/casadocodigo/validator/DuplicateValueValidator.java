2533fca215836ee215fef0807f76e1fe220711e4
mickael <mickaelluiz843@gmail.com>
2021-03-16 11:15:03 -0300

    refatorando a implementação da anotação @UniqueValue

--- src/main/java/com/mikkaeru/casadocodigo/validator/DuplicateValueValidator.java
+++ src/main/java/com/mikkaeru/casadocodigo/validator/DuplicateValueValidator.java
@@ -1,24 +1,19 @@
 package com.mikkaeru.casadocodigo.validator;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.stereotype.Component;
-import org.springframework.util.Assert;
-
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Locale;
 
-@Component
 public class DuplicateValueValidator implements ConstraintValidator<UniqueValue, Object> {
 
     private Class<?> aClass;
     private String domainAttribute;
 
-    @Autowired
-    private JdbcTemplate jdbcTemplate;
+    @PersistenceContext
+    private EntityManager manager;
 
     @Override
     public void initialize(UniqueValue constraintAnnotation) {
@@ -28,28 +23,9 @@ public class DuplicateValueValidator implements ConstraintValidator<UniqueValue,
 
     @Override
     public boolean isValid(Object value, ConstraintValidatorContext constraintValidatorContext) {
-        String[] tmp = aClass.getName().split("\\.");
-        String klassName = tmp[tmp.length - 1].toLowerCase(Locale.ROOT);
-
-        String sql = "SELECT * FROM " + klassName + " WHERE " + domainAttribute + "=" + "'" +value+ "'";
-
-        List<String> list = jdbcTemplate.query(sql, rs -> {
-
-            List<String> listTmp = new ArrayList<>();
-
-            while (rs.next()) {
-
-                var field = rs.getString(domainAttribute);
-
-                listTmp.add(field);
-
-            }
-
-            return listTmp;
-        });
-
-        assert list != null;
-        Assert.state(list.size() <= 1, "Foi encontrado mais de um " + aClass + " com o atributo " + domainAttribute+" = " + value);
+        Query query = manager.createQuery("SELECT 1 FROM " + aClass.getName() + " WHERE " +domainAttribute+ "=:value");
+        query.setParameter("value", value);
+        List<?> list = query.getResultList();
 
         return list.isEmpty();
     }
